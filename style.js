4c2c6d16370821214b716f4e5daade9492eb5f2b
Adhyatm2717 <adhyatmmudgal@gmail.com>
2025-12-30 02:08:33 +0530

    js and favicon

---
+++ style.js
@@ -0,0 +1,14 @@
+const display = document.getElementById("display")
+
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(){
+    display.value = ""
+}
+function del(){
+    display.value = display.value.toString().slice(0, -1)
+}
+function calculate(){
+    display.value = eval(display.value)
+}
